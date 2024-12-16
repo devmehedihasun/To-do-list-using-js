f1f2f2e30c736952cfc6e0050a8745cc9e4bc599
Mehedi Hasan <117835267+devmehedihasun@users.noreply.github.com>
2024-12-16 12:35:18 +0600

    to do list done perfectly

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
 
@@ -21,13 +22,22 @@ function addTask(){
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 });
 
 
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
 
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
 
+showTask();
 
