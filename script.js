59def64b3a0563c0d47c470c4c059c2eae0e0318
Mehedi Hasan <117835267+devmehedihasun@users.noreply.github.com>
2024-12-16 11:57:24 +0600

    1st commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+}
